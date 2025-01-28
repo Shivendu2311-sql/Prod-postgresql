1e82b016f343de7d39677090d472cb21c331f640
Shivendu2311-sql <shivendu@t.digital>
2025-01-28 19:54:43 +0530

    Automated file upload

---
+++ prod/Script-10.sql
@@ -0,0 +1,25 @@
+select * from public.get_agent_and_team_counts('005Pr000007rXHxIAM',
+'2025-01-01','2025-01-31','{}','all',array['e1122a86-cc34-48b7-b5a2-0e6fa811e9d8'])
+
+select * from public.get_agent_and_team_counts('005Pr000007rXHxIAM',
+'2025-01-01','2025-01-31','{}','all',array['23aa5a52-74a0-4889-9565-86dac0b49e6e'])
+
+select * from public.get_agent_and_team_counts('005Pr000007rXHxIAM',
+'2025-01-10','2025-01-17','{}','all')
+--005Pr0000081iX3IAI
+select * from public.get_agent_and_team_counts('005Pr0000081iX3IAI',
+'2025-01-10','2025-01-17','{}','all')
+
+select * from public.get_lead_counts_nds_2_test_shiva('005Pr0000081iX3IAI','2025-01-09','2025-01-17', '{}','all')
+
+select * from public.get_agent_and_team_counts('0055c00000AypyrAAB',
+'2025-01-10','2025-01-17','{}','all')
+
+select * from public.get_lead_counts_nds_2('005Pr0000081iX3IAI',
+'2025-01-10','2025-01-17','{}','all')
+
+select * from public.get_lead_counts_nds_2('005Pr000007rXHxIAM',
+'2025-01-17','2025-01-27','{}','all')
+
+select * from public.get_agent_and_team_counts('005Pr000007rXHxIAM',
+'2025-01-17','2025-01-27','{}','all')
